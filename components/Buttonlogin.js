2c8e3713ae864ea5ed5b574667929a0376bd1ad4
1poubelle <1poubelle@pm.me>
2025-08-10 12:17:04 +0100

    ok

--- components/Buttonlogin.js
+++ components/Buttonlogin.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const Buttonlogin = ({ IsLoggedIn, Name, extraStyle }) => {
+const Buttonlogin = ({ IsLoggedIn, Name, extraStyle? }) => {
   console.log(extraStyle);
   if (IsLoggedIn) {
     return (
