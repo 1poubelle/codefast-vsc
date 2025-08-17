f7558321e4ba84ead73f99e19b28aa449b518b64
1poubelle <1poubelle@pm.me>
2025-08-17 13:19:58 +0100

    signOut

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,10 @@
+import ButtonLogout from "@/components/Buttonlogout";
 
 export default function dashboard() {
   return (
    <main>
     <h1>dashboard</h1>
-    
+    <ButtonLogout />
 
     </main>
   );
